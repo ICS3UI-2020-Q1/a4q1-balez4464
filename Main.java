01e44d5bee66299fc63a0afb88dad27a461fdb71
balez4464 <balez4464@wrdsb.ca>
2020-10-02 13:55:59 +0000

    Created a program to count down by 5 until it reaches users number

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 /**
- *
- * @author 
+ *This program counts down from 100 by until it reaches users number
+ * @author Zachary Balean
  */
 public class Main {
 
@@ -9,7 +11,21 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // creates Scanner
+    Scanner input = new Scanner(System.in);
+    
+    //promts user for number
+    System.out.println("Please enter an integer to count down to");
+    //users number and number for counting down
+    int userNumber = input.nextInt();
+    int countDown = 100;
+
+    //while loop for counting down
+    System.out.println("Count Down:");
+    while(countDown >= userNumber){
+      System.out.println(countDown);
+      countDown = countDown - 5;
+    }
     
   }
 }
